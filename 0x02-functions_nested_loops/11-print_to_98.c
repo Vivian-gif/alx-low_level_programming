a09bc5940fff963cba689ff612c1abe37184e38a
lucheey V <voluchi06@gmail.com>
2022-03-16 02:28:43 +0100

    Nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- *ger
- * Description: function that prints all natur print_to_98 - Check Holberton
- * @n: A input inteal numbers
+ * print_to_98 - Check Holberton
+ * @n: A input integer
+ * Description: function that prints all natural numbers
  * from n to 98, followed by a new line.
  * Return: Nothing
  */
