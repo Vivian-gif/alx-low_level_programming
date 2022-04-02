828476f63d0c757571a4eab6ea78c1b7359e7cda
lucheey V <voluchi06@gmail.com>
2022-04-02 03:21:37 +0100

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print the unt
+ * main - print the num of args you passed to it
+ * @argc: argument count
  * @argv: argument vector, array of strings
- *num of args you passed to it
- * @argc: argument co Return: 0
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
